b8eaf041058428b07a230c7e9f5a660f325ee6ca
Mouadh <ktarimouadh@gmail.com>
2024-05-05 22:26:55 +0100

    add priest death animation, update enemy HitBox script

--- Assets/Script/EnemyScripts/HitBox.cs
+++ Assets/Script/EnemyScripts/HitBox.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 public class HitBox : MonoBehaviour
 {
     public Transform healthBar;
+    Transform sight;
     public float hpAmount, speed;
     float reducedHP, healthPosition;
     public bool isDead = true;
+    float time = 0f;
     // Start is called before the first frame update
     void Awake()
     {
         reducedHP = healthBar.lossyScale.y / hpAmount;
         healthPosition = healthBar.lossyScale.y;
     }
+    void Start(){
+        sight = GetComponent<Chasing>().EnemySight;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (healthBar.lossyScale.y <= 0f){
-            isDead = true;
+            bool isPlaying = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Death");
+            if (isPlaying == false){
+                GetComponent<Animator>().SetBool("Dead", true);
+                sight.GetComponent<FieldofView>().Animate();   
+            }
+            if (time >= GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length)
+                Destroy(gameObject);
+            time += Time.deltaTime;
+
             
         }
     }
